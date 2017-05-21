9bed6c8e0520ed944a623c8f24929724625ffd29
pranaypushkar92@gmail.com <pranaypushkar92@gmail.com>
2017-05-21 22:45:26 +0530

    New structure for solution

--- main.cpp
+++ main.cpp
@@ -1,45 +1,7 @@
-/* 
- The problem statement can be found on https://www.codechef.com/problems/HS08TEST */
-
-/*  Author   - PRANAY PUSHKAR
- *  Date     - 21 May 2017
- *  IDE      - Codeblocks
- *  Language - C++
- */
-
-#include<iostream>
-#include<iomanip>
-using namespace std;
-
+void ATM_HS08TEST();
 int main()
 {
-    int x;
-    float y;
+    ATM_HS08TEST();
 
-    cin >> x >> y;
-
-    if(0 < x && x <= 2000)
-    {
-        if(0 <= y && y <= 2000)
-        {
-            if(x > y ) //Test case passed
-            {
-                cout << fixed << setprecision(2) << y;
-            }
-            else if(x%5 == 0 && y > (x+.5)) //Test case passed
-            {
-                cout << fixed << setprecision(2) << (float)((y-x)-(.5));
-            }
-            else if (x%5 != 0)
-            {
-                cout << fixed << setprecision(2) << y; //Test case passed
-            }
-            else
-            {
-                cout << fixed << setprecision(2) << y; //Test case passed
-            }
-
-        }
-    }
     return 0;
 }
